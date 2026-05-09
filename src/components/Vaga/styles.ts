9cf5d743cd430dabfea5191d69deccf9cf101214
MAX <max@roslaniec.com>
2026-05-09 12:56:31 -0300

    fix: adicionar variáveis CSS (:root) ao createGlobalStyle para restaurar estilização original

--- src/components/Vaga/styles.ts
+++ src/components/Vaga/styles.ts
@@ -1,28 +1,5 @@
 import styled from 'styled-components'
 
-export const VagaLink = styled.a`
-  border-color: var(--cor-secundaria);
-  background-color: var(--cor-principal);
-  color: var(--cor-secundaria);
-  display: inline-block;
-  padding: 8px 16px;
-  text-decoration: none;
-  margin-top: 16px;
-  font-weight: bold;
-  font-size: 14px;
-  border-radius: 8px;
-  text-align: center;
-
-  @media (max-width: 768px) {
-    display: block;
-  }
-`
-
-export const VagaTitulo = styled.h3`
-  font-weight: bold;
-  margin-bottom: 16px;
-`
-
 export const VagaContainer = styled.li`
   border: 1px solid var(--cor-principal);
   background-color: var(--cor-secundaria);
@@ -40,5 +17,28 @@ export const VagaContainer = styled.li`
       background-color: var(--cor-secundaria);
       color: var(--cor-principal);
     }
+  }
+`
+
+export const VagaTitulo = styled.h3`
+  font-weight: bold;
+  margin-bottom: 16px;
+`
+
+export const VagaLink = styled.a`
+  border-color: var(--cor-secundaria);
+  background-color: var(--cor-principal);
+  color: var(--cor-secundaria);
+  display: inline-block;
+  padding: 8px 16px;
+  text-decoration: none;
+  margin-top: 16px;
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 8px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    display: block;
   }
 `
--- src/theme/global.ts
+++ src/theme/global.ts
@@ -1,6 +1,11 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 const EstiloGlobal = createGlobalStyle`
+  :root {
+    --cor-principal: #a7727d;
+    --cor-secundaria: #f9f5e7;
+  }
+
   * {
     margin: 0;
     padding: 0;
